48d466c5ac24a4eae6b7550b853f96a0b50905a1
nhsvaerd <nh@svrd.no>
2026-01-06 22:19:14 +0100

    custom endpoints

--- PocketBase/pb_hooks/event.create.js
+++ PocketBase/pb_hooks/event.create.js
@@ -36,6 +36,38 @@ routerAdd(
         const startTime = input.startTime;
         const ownerEmail = input.ownerEmail
 
+        function createNewRegistrant(eventId, registrantName, registrantEmail = "", isHost = false) {
+            let record = new Record(e.app.findCollectionByNameOrId("registrants"))
+
+            const password = $security.randomString(24);
+            const inviteId = $security.randomString(12);
+            const inviteCode = `${inviteId}.${password}`;
+
+            record.setPassword(password);
+            record.set("invite_id", inviteId);
+            record.set("event", eventId);
+            record.set("name", registrantName);
+            if(!isBlank(registrantEmail)) { 
+                record.set( "registrant_email", registrantEmail ) 
+            };
+            record.set("is_host", isHost);
+            
+            try {
+                e.app.save(record);
+            } catch (err) {
+                throwApi(
+                    500,
+                    "Unknown error"
+                );
+            }
+
+            return {
+                recordId: record.id,
+                inviteId: inviteId,
+                inviteCode: inviteCode
+            }
+        };        
+
         function createNewEvent(eventTitle, startTime,) {
             let record = new Record(e.app.findCollectionByNameOrId("events"))
 
